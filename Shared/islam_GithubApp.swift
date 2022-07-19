40b6bf7920391f8430676d96d3d4d7788913ca37
islam <islamxdeveloper@gmail.com>
2022-07-19 14:23:44 +0400

    add new VC

---
+++ Shared/islam_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  islam_GithubApp.swift
+//  Shared
+//
+//  Created by Saeed Alshamsi on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct islam_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
